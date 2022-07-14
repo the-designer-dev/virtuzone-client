c61058e9fd5ac60799df3522dc5bd2b296f8c0fb
Ali-Muhammad-123 <Alim.9171@gmail.com>
2022-07-14 17:14:44 +0500

    added

--- pages/signIn.js
+++ pages/signIn.js
@@ -11,16 +11,36 @@ import LinearGradient from 'react-native-linear-gradient';
 export default function SignIn({navigation}) {
   return (
     <View>
-      <View>
-        <ImageBackground
-          source={require('../images/signIn.png')}
-          style={{width: '100%', height: '100%'}}>
-          <Text>Sign In</Text>
-          <Text>To Account</Text>
-        </ImageBackground>
-      </View>
+      <ImageBackground
+        source={require('../images/signIn.png')}
+        style={{width: '100%', height: 300}}>
+        <View style={styles.topheader}>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>Sign In</Text>
+            <Text style={[styles.textStyle, {paddingBottom: 20}]}>
+              To Account
+            </Text>
+            <Text style={styles.textStyle2}>
+              Sign with username or email and password to use your account.
+            </Text>
+          </View>
+        </View>
+      </ImageBackground>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  topheader: {
+    height: 300,
+    padding: 24,
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  textStyle: {fontSize: 35, fontWeight: 'bold', color: '#FFF'},
+  textStyle2: {fontSize: 16, fontWeight: '400', color: '#FFF'},
+  bottomSection: {
+    backgroundColor: '#f1f1f1',
+  },
+});
